5fc5fe68b61f4aebeb657c702d8642a408cd1cd4
ahmedabdalmageed95 <ahmedabdalmageed95@yahoo.com>
2017-08-15 12:59:15 +0200

    new commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -15,6 +15,7 @@ export class ContactPage implements OnInit {
   Newsbody: Newsdetailsresponse;
   showdetails=false;
   date: Date= new Date('2017-7-1');
+  elements :Element;
   constructor(public navCtrl: NavController , private CompanyService:CompanyService) {
         this.CompanyService.getnews(this.date,100,false).subscribe(data  => {this.News = data;
           // console.log(this.News);
@@ -27,7 +28,13 @@ ngOnInit() {
 getdetails(id:string){
   const parsed = Number(id);
  this.CompanyService.getnewsdetails(parsed).subscribe(data  => {this.Newsbody = data;
-          // console.log(this.News);
+              var div = document.createElement('div');
+              div.innerHTML = this.Newsbody.result.V[3];
+               this.elements = div;
+               // document.writeln(this.elements.innerHTML);
+               console.log(this.elements);
+               document.getElementById('id').innerHTML = this.elements.innerHTML;
+              // console.log(this.News);
            } );
  this.showdetails=true;
 }
