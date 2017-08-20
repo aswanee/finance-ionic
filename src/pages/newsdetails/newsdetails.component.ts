0bd8821f388c0b4ae46d698e80356a1123118e42
misMacBookPro <mis@misMacBookPro-2.local>
2017-08-20 11:21:19 +0200

    news style changes

--- src/pages/newsdetails/newsdetails.component.ts
+++ src/pages/newsdetails/newsdetails.component.ts
@@ -1,49 +1,48 @@
-import { Component , Input, Output,OnInit } from '@angular/core';
-import {CompanyService} from './../../app/company.service';
-import {News} from './../../app/news.interface';
-import {Newsbody} from './../../app/newsbody.interface';
-import {Newsresponse} from './../../app/newsresponse.interface';
-import {Newsdetailsresponse} from './../../app/newsdetailsresponse.interface';
-import {Observable} from 'rxjs/Rx';
+import { Component, Input, Output, OnInit } from "@angular/core";
+import { CompanyService } from "./../../app/company.service";
+import { News } from "./../../app/news.interface";
+import { Newsbody } from "./../../app/newsbody.interface";
+import { Newsresponse } from "./../../app/newsresponse.interface";
+import { Newsdetailsresponse } from "./../../app/newsdetailsresponse.interface";
+import { Observable } from "rxjs/Rx";
 @Component({
-    // moduleId: module.id,
-    selector: 'newsdetails',
-    templateUrl: 'newsdetails.component.html',
-    // styleUrls: ['newsdetails.component.scss']
+  // moduleId: module.id,
+  selector: "newsdetails",
+  templateUrl: "newsdetails.component.html"
+  // styleUrls: ['newsdetails.component.scss']
 })
 export class NewsdetailsComponent implements OnInit {
-@Input()id: string;
-Newsbody:Newsdetailsresponse;
-elements:Element;
-constructor(private CompanyService:CompanyService){
-
-}
-ngOnInit() {
-  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-  //Add 'implements OnInit' to the class.
+  @Input() id: string;
+  Newsbody: Newsdetailsresponse;
+  elements: Element;
+  constructor(private CompanyService: CompanyService) {}
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
     const parsed = Number(this.id);
- this.CompanyService.getnewsdetails(parsed).subscribe(data  => {this.Newsbody = data;
-              var div = document.createElement('div');
-              div.innerHTML = this.Newsbody.result.V[3];
-               this.elements = div;
-               // document.writeln(this.elements.innerHTML);
-               console.log(this.elements);
-               document.getElementById('id').innerHTML = this.elements.innerHTML;
-              // console.log(this.News);
-           } );
- // this.showdetails=true;
-}
-// getdetails(){
-//   const parsed = Number(this.id);
-//  this.CompanyService.getnewsdetails(parsed).subscribe(data  => {this.Newsbody = data;
-//               var div = document.createElement('div');
-//               div.innerHTML = this.Newsbody.result.V[3];
-//                this.elements = div;
-//                // document.writeln(this.elements.innerHTML);
-//                console.log(this.elements);
-//                document.getElementById('id').innerHTML = this.elements.innerHTML;
-//               // console.log(this.News);
-//            } );
-//  // this.showdetails=true;
-// }
+    this.CompanyService.getnewsdetails(parsed).subscribe(data => {
+      this.Newsbody = data;
+      var div = document.createElement("div");
+      div.innerHTML = this.Newsbody.result.V[3];
+      this.elements = div;
+      // document.writeln(this.elements.innerHTML);
+      console.log(this.elements);
+      document.getElementById("id").innerHTML = this.elements.innerHTML;
+      // console.log(this.News);
+    });
+    // this.showdetails=true;
+  }
+  // getdetails(){
+  //   const parsed = Number(this.id);
+  //  this.CompanyService.getnewsdetails(parsed).subscribe(data  => {this.Newsbody = data;
+  //               var div = document.createElement('div');
+  //               div.innerHTML = this.Newsbody.result.V[3];
+  //                this.elements = div;
+  //                // document.writeln(this.elements.innerHTML);
+  //                console.log(this.elements);
+  //                document.getElementById('id').innerHTML = this.elements.innerHTML;
+  //               // console.log(this.News);
+  //            } );
+  //  // this.showdetails=true;
+  // }
 }
