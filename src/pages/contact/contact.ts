42ea8215d131967b86f6dd13d42ff0a3ae9f3b2d
ahmedabdalmageed95 <ahmedabdalmageed95@yahoo.com>
2017-08-20 11:13:13 +0200

    newscommit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,37 +1,50 @@
-import { Component , OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {CompanyService} from './../../app/company.service';
-import {News} from './../../app/news.interface';
-import {Newsbody} from './../../app/newsbody.interface';
-import {Newsresponse} from './../../app/newsresponse.interface';
-import {Newsdetailsresponse} from './../../app/newsdetailsresponse.interface';
-import {Observable} from 'rxjs/Rx';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { CompanyService } from "./../../app/company.service";
+import { News } from "./../../app/news.interface";
+import { Newsbody } from "./../../app/newsbody.interface";
+import { Newsresponse } from "./../../app/newsresponse.interface";
+import { Newsdetailsresponse } from "./../../app/newsdetailsresponse.interface";
+import { Observable } from "rxjs/Rx";
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: "page-contact",
+  templateUrl: "contact.html"
 })
 export class ContactPage implements OnInit {
   News: Newsresponse;
   Newsbody: Newsdetailsresponse;
-  showdetails=false;
-  date: Date= new Date('2017-7-1');
-  constructor(public navCtrl: NavController , private CompanyService:CompanyService) {
-        this.CompanyService.getnews(this.date,100,false).subscribe(data  => {this.News = data;
-          // console.log(this.News);
-           } );
+  showdetails = false;
+  date: Date = new Date("2017-7-1");
+  elements: Element;
+  id: string;
+  constructor(
+    public navCtrl: NavController,
+    private CompanyService: CompanyService
+  ) {
+    this.CompanyService.getnews(this.date, 100, false).subscribe(data => {
+      this.News = data;
+      // console.log(this.News);
+    });
   }
-ngOnInit() {
-  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-  //Add 'implements OnInit' to the class.
-}
-getdetails(id:string){
-  const parsed = Number(id);
- this.CompanyService.getnewsdetails(parsed).subscribe(data  => {this.Newsbody = data;
-          // console.log(this.News);
-           } );
- this.showdetails=true;
-}
-back(){
-  this.showdetails=false;
-}
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+  getdetails(id) {
+    this.id = id;
+    //   const parsed = Number(id);
+    //  this.CompanyService.getnewsdetails(parsed).subscribe(data  => {this.Newsbody = data;
+    //               var div = document.createElement('div');
+    //               div.innerHTML = this.Newsbody.result.V[3];
+    //                this.elements = div;
+    //                // document.writeln(this.elements.innerHTML);
+    //                console.log(this.elements);
+    //                document.getElementById('id').innerHTML = this.elements.innerHTML;
+    //               // console.log(this.News);
+    //            } );
+    this.showdetails = true;
+  }
+  back() {
+    this.showdetails = false;
+  }
 }
