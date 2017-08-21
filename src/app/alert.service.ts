b4630187a396268e394d1f5a24a6a91050211cb7
ahmedabdalmageed95 <ahmedabdalmageed95@yahoo.com>
2017-08-21 16:49:25 +0200

    deletealert

--- src/app/alert.service.ts
+++ src/app/alert.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { Http, Response } from "@angular/http";
 import { alertresponse } from "./alert.interface";
+import { deleteresponse } from "./delete.interface";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/catch";
@@ -29,6 +30,19 @@ export class AlertService extends ParentService {
       .get(this.link)
       .map(x => {
         return <alertresponse>x.json();
+      })
+      .catch((t: Response) => t.json());
+  }
+  deletealerts(id: number): Observable<deleteresponse> {
+    this.link =
+      "http://staging5.arabfinance.com/apis/account/DeleteAlert?alertIDs=2817";
+    // console.log(link);
+    this.link = this.link + id;
+    console.log(this.link);
+    return this.http
+      .get(this.link)
+      .map(x => {
+        return <deleteresponse>x.json();
       })
       .catch((t: Response) => t.json());
   }
