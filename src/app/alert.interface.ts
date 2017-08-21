1244002becd35c3cbc3cc7b7f31f5902bc8d0563
ahmedabdalmageed95 <ahmedabdalmageed95@yahoo.com>
2017-08-21 15:22:02 +0200

    enums

--- src/app/alert.interface.ts
+++ src/app/alert.interface.ts
@@ -6,9 +6,9 @@ interface alert {
   AlertID: number;
   UserID: number;
   Code: string;
-  Type: number;
-  Field: number;
-  Criteria: number;
+  Type: Type;
+  Field: Field;
+  Criteria: Criteria;
   Value: number;
   SetTime: string;
   MetTime: string;
@@ -18,4 +18,34 @@ interface alert {
   LastUpdated: string;
   IsDeleted: boolean;
   DoubleValue: number;
+}
+enum Type {
+  AlertStockTypeIndex = 1,
+  AlertStockTypeStock = 2,
+  AlertStockTypeOTC = 3
+}
+enum Field {
+  AlertFieldLastTrade = 1,
+  AlertFieldNetChange = 2,
+  AlertFieldPercentageChange = 3,
+  AlertFieldVWAP = 4,
+  AlertFieldBestBid = 5,
+  AlertFieldBestAsk = 6,
+  AlertFieldBidSize = 7,
+  AlertFieldAskSize = 8,
+  AlertFieldVolume = 9,
+  AlertFieldTurnOver = 10,
+  AlertFieldTransactions = 11,
+  AlertFieldHigh = 12,
+  AlertFieldLow = 13,
+  AlertFieldTotalBidSize = 14,
+  AlertFieldTotalAskSize = 15,
+  AlertFieldValue = 16,
+  AlertFieldIntradayHigh = 17,
+  AlertFieldIntradayLow = 18
+}
+enum Criteria {
+  AlertCriteriaLessThan = 1,
+  AlertCriteriaEqual = 2,
+  AlertCriteriaGreaterThan = 3
 }
