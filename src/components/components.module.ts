3e21105ad950f12b973ebd0704f810e6af188804
Mohamed Rashed <bnrashed@gmail.com>
2017-11-06 21:02:12 +0200

    delete un lyze laod

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CustNavComponent } from './cust-nav/cust-nav';
-@NgModule({
-	declarations: [CustNavComponent],
-	imports: [],
-	exports: [CustNavComponent]
-})
-export class ComponentsModule {}
--- src/pages/pop-over/pop-over.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { IonicPageModule } from "ionic-angular";
-import { PopoverPage } from "./pop-over";
-import { LanguagePipe } from "./../../pipes/Language/Language.pipe";
-@NgModule({
-  declarations: [],
-  imports: [IonicPageModule.forChild(PopoverPage)]
-})
-export class PopOverPageModule {}
